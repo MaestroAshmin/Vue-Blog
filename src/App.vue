a4e3fcc2c42ae3c16d5430bbc7cdc0575ac3fa1d
Ashmin Karki <ashmin.karki1996@gmail.com>
2019-10-10 16:51:19 +0545

    Http and Get Requests

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <show-blog></show-blog>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addBlog from "./components/addBlog.vue";
+import showBlog from "./components/showBlog.vue";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    "add-blog": addBlog,
+    "show-blog": showBlog
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
